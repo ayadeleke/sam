6a603a8ade1149100a83055ce7b8bf7a7a1ec419
ayadeleke <a.adeleke@alustudent.com>
2024-04-12 11:37:58 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Enable CORS for all routes
+app.use(cors());
+
+// Express now includes built-in JSON parsing
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://sbabalola:Samuel2006@cluster0.xpc6u25.mongodb.net/dvei');
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+    console.log('Connected to MongoDB');
+});
+
+// Registration endpoint
+app.post('/register', async (req, res) => {
+    const { email, password, name, role } = req.body;
+
+    try {
+        const existingUser = await User.findOne({ email: email.toLowerCase() });
+
+        if (existingUser) {
+            return res.status(400).json({ error: "Email already in use." });
+        }
+
+        // Create a new user in the database with the hashed password
+        const user = new User({
+            email: email.toLowerCase(),
+            password: password,
+            name,
+            role
+        });
+        await user.save();
+
+        res.setHeader('Content-Type', 'text/html');
+        res.json({ message: 'Registration successful, please login'});
+    } catch (error) {
+        console.error('Registration error:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+
+// Login endpoint
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        // Check if the email exists in the database
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(401).json({ error: 'Invalid email or password. Please try again.' });
+        }
+
+        // Compare the provided password with the hashed password in the database
+        const passwordMatch = await bcrypt.compare(password, user.password);
+
+        if (passwordMatch) {
+            res.json({ message: 'Login successful' });
+        } else {
+            res.status(401).json({ error: 'Invalid email or password. Please try again.' });
+        }
+    } catch (error) {
+        console.error('Login error:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
